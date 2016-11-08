cb0aca54618058afab63cdf73b43a2ddcf5d81df
Unknown <milton_st@hotmail.com>
2016-11-07 23:15:42 -0300

    Inicio
     Se instalo framework, dependencias y codigo

---
+++ cotillon/application/views/includes/header.php
@@ -0,0 +1,15 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://localhost/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://localhost/assets/css/font-awesome.min.css">
+        <title>Cotillon</title>
+    </head>
+    <body>
+    <div>
---
+++ cotillon/application/views/pages/inicio/prueba.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<h1>hola</h1>
+
+
+<?php 
+    echo anchor( base_url("inicio/salir"), "Salir", "Salir del sistema");
+?>
